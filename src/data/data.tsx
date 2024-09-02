0babc0acbe1cf3d77772c68d648e3913a40315cc
Rudra <rudra@grellmann.eu>
2024-09-02 10:13:09 -0300

    Update Homepage-Meta

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -44,8 +44,8 @@ import {
  * Page meta data
  */
 export const homePageMeta: HomepageMeta = {
-  title: 'React Resume Template',
-  description: "Example site built with Tim Baker's react resume template",
+  title: 'Martin (Rudra) Grellmann',
+  description: 'Curriculum vitae of Martin (Rudra) Grellmann',
 };
 
 /**
