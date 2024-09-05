0e292c2e6fdec9f818effccb68113b02ce19dee3
Rudra <rudra@grellmann.eu>
2024-09-05 08:29:20 -0300

    Remove the Contact-form and Add an email-button

--- src/components/Sections/Contact/index.tsx
+++ src/components/Sections/Contact/index.tsx
@@ -10,7 +10,6 @@ import InstagramIcon from '../../Icon/InstagramIcon';
 import LinkedInIcon from '../../Icon/LinkedInIcon';
 import TwitterIcon from '../../Icon/TwitterIcon';
 import Section from '../../Layout/Section';
-import ContactForm from './ContactForm';
 
 const ContactValueMap: Record<ContactType, ContactValue> = {
   [ContactType.Email]: {Icon: EnvelopeIcon, srLabel: 'Email'},
@@ -33,11 +32,8 @@ const Contact: FC = memo(() => {
           <h2 className="text-2xl font-bold text-white">{headerText}</h2>
         </div>
         <div className="grid grid-cols-1 gap-6 md:grid-cols-2">
+          <p className="prose leading-6 text-neutral-300">{description}</p>
           <div className="order-2 col-span-1 md:order-1 ">
-            <ContactForm />
-          </div>
-          <div className="order-1 col-span-1 flex flex-col gap-y-4 md:order-2">
-            <p className="prose leading-6 text-neutral-300">{description}</p>
             <dl className="flex flex-col space-y-4 text-base text-neutral-500 sm:space-y-2">
               {items.map(({type, text, href}) => {
                 const {Icon, srLabel} = ContactValueMap[type];
@@ -61,6 +57,21 @@ const Contact: FC = memo(() => {
               })}
             </dl>
           </div>
+        </div>
+        <div className="flex flex-col items-stretch mt-4">
+          <a href="mailto:martin@grellmann.eu?subject=Send from grellmann.app CV">
+            <button
+              className="inline-flex items-center justify-center text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-base px-8 py-4 me-2 mb-2 border border-cyan-500 w-full"
+              type="button">
+              <svg aria-hidden="true" className="w-6 h-6 text-white me-2 ml-2" fill="currentColor"
+                   viewBox="0 0 20 16" xmlns="http://www.w3.org/2000/svg">
+                <path d="m10.036 8.278 9.258-7.79A1.979 1.979 0 0 0 18 0H2A1.987 1.987 0 0 0 .641.541l9.395 7.737Z" />
+                <path
+                  d="M11.241 9.817c-.36.275-.801.425-1.255.427-.428 0-.845-.138-1.187-.395L0 2.6V14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2.5l-8.759 7.317Z" />
+              </svg>
+              Send an eMail
+            </button>
+          </a>
         </div>
       </div>
     </Section>
