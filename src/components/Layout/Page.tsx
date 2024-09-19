f80186e469958296dc8461d9621883afa6a3a0b0
Rudra <rudra@grellmann.eu>
2024-09-18 21:31:06 -0300

    Change fixes in page

--- src/components/Layout/Page.tsx
+++ src/components/Layout/Page.tsx
@@ -15,19 +15,11 @@ const Page: NextPage<PropsWithChildren<HomepageMeta>> = memo(({children, title, 
         <meta content={description} name="description" />
 
         {/* several domains list the same content, make sure google knows we mean this one. */}
-        <link href={`https://www.grellmann.app${pathname}`} key="canonical" rel="canonical" />
+        <link href={`https://grellmann.app${pathname}`} key="canonical" rel="canonical" />
 
         <link href="/favicon.ico" rel="icon" sizes="any" />
         <link href="/icon.svg" rel="icon" type="image/svg+xml" />
         <link href="/apple-touch-icon.png" rel="apple-touch-icon" />
-        <link href="/apple-touch-icon-57x57.png" rel="apple-touch-icon" sizes="57x57" />
-        <link href="/apple-touch-icon-72x72.png" rel="apple-touch-icon" sizes="72x72" />
-        <link href="/apple-touch-icon-76x76.png" rel="apple-touch-icon" sizes="76x76" />
-        <link href="/apple-touch-icon-114x114.png" rel="apple-touch-icon" sizes="114x114" />
-        <link href="/apple-touch-icon-120x120.png" rel="apple-touch-icon" sizes="120x120" />
-        <link href="/apple-touch-icon-144x144.png" rel="apple-touch-icon" sizes="144x144" />
-        <link href="/apple-touch-icon-152x152.png" rel="apple-touch-icon" sizes="152x152" />
-        <link href="/apple-touch-icon-180x180.png" rel="apple-touch-icon" sizes="180x180" />
 
         <link href="/site.webmanifest" rel="manifest" />
 
