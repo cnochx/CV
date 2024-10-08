82102166faa59130414d02d5c7326cd6ac4b030f
Rudra <rudra@grellmann.eu>
2024-10-08 11:15:47 -0300

    Small Fix

--- src/data/homepageData.tsx
+++ src/data/homepageData.tsx
@@ -4,15 +4,15 @@ import {HomePageMeta} from './homepageDataDef';
  * Page meta data
  */
 export const homePageMeta: HomePageMeta = {
-  title: 'Full-Stack software engineer  Martin (Rudra) Grellmann',
-  description: 'CV of Martin (Rudra) Grellmann, Full-Stack software engineer, with the Knowledge as SAP Technical Consultant for Business Software Development & Industry 4.0 Applications who is committed to continuous learning and innovation.',
+  title: 'Full-stack software engineer  Martin (Rudra) Grellmann',
+  description: 'CV of Martin (Rudra) Grellmann, full-stack software engineer, with knowledge as an SAP technical consultant for business software development and Industry 4.0 applications, who is committed to continuous learning and innovation.',
   image: 'https://www.grellmann.app/images/profilepic.jpg',
   url: 'https://www.grellmann.app',
   ogImg: 'https://www.grellmann.app/images/profilepic_og.jpg',
   ogImgType: 'image/jpeg',
   ogImgWidth: '1200',
   ogImgHeight: '630',
-  ogImgAlt: 'Martin Grellmann, Full-Stack software engineer',
+  ogImgAlt: 'Martin Grellmann, Full-stack software engineer',
   ogType: 'profile',
   ogLinkPreView: 'CV Martin Grellmann',
   twitterCardType: 'summary_large',
