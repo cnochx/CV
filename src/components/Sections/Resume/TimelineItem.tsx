d2f036027911603c8abb795473ee9539285aab8c
Rudra <rudra@grellmann.eu>
2024-10-09 18:46:48 -0300

    Fix props

--- src/components/Sections/Resume/TimelineItem.tsx
+++ src/components/Sections/Resume/TimelineItem.tsx
@@ -49,8 +49,8 @@ const TimelineItem: FC<{item: TimelineItem}> = memo(({item}) => {
               )}
               href={href}
               key={text}
-              target="_blank"
               rel="noopener noreferrer"
+              target="_blank"
             >
               {text}
               {Icon && <Icon className="h-5 w-5 sm:h-6 sm:w-6" />}
