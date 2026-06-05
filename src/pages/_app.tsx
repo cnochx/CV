51d68340db595ab5652fdc7f31f4b4b2fbd7a49f
Rudra <rudra@grellmann.eu>
2026-06-05 15:19:43 +0200

    Integrate Viewport

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,15 @@ import 'tailwindcss/tailwind.css';
 import '../globalStyles.scss';
 
 import type {AppProps} from 'next/app';
+import Head from 'next/head';
 import {memo} from 'react';
 
 const MyApp = memo(({Component, pageProps}: AppProps): JSX.Element => {
   return (
     <>
+      <Head>
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+      </Head>
       <Component {...pageProps} />
     </>
   );
